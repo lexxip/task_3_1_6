5280cd09944c45f51d42b002af5abd16990727c4
lexxip <119631103+lexxip@users.noreply.github.com>
2023-03-18 14:57:41 +0500

    Add files via upload

---
+++ src/main/java/ru/preproject/task_3_1_6/Task316Application.java
@@ -0,0 +1,13 @@
+package ru.preproject.task_3_1_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task316Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task316Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/preproject/task_3_1_6/Task316ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.preproject.task_3_1_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task316ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
